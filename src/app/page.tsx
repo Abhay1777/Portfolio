3d99db7c9bcfb61db8da9bdaa3517ece9f38b7f5
Abhay <fcj520587@gmail.com>
2026-05-31 14:00:53 +0530

    style: remove custom cursor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,14 +7,12 @@ import Contact from "@/components/Contact";
 import Footer from "@/components/Footer";
 import Preloader from "@/components/Preloader";
 import ScrollProgress from "@/components/ScrollProgress";
-import CustomCursor from "@/components/CustomCursor";
 
 export default function Home() {
   return (
     <>
       <Preloader />
       <ScrollProgress />
-      <CustomCursor />
       <Header />
       <main className="flex-grow w-full overflow-x-hidden">
         <Hero />
